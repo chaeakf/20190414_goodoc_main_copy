80689f37a526d4ad9e8b1ce6b3d261aca0b893a2
Eunsil-Jeong <daramchu@naver.com>
2019-04-14 14:37:47 +0900

    연습문제/ 응운문제까지 작성

--- app/src/main/java/com/tjeit/a20190414_goodoc_main_copy/MainActivity.java
+++ app/src/main/java/com/tjeit/a20190414_goodoc_main_copy/MainActivity.java
@@ -2,12 +2,39 @@ package com.tjeit.a20190414_goodoc_main_copy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText inputSearch;
+    Button btnSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputSearch = findViewById(R.id.inputSearch);
+        btnSearch = findViewById(R.id.btnSearch);
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String searchInput = inputSearch.getText().toString();
+                String toastMessage = String.format("[%s]을 검색합니다",searchInput);
+
+                //Toast.makeText(MainActivity.this, searchInput, Toast.LENGTH_SHORT).show();
+                //Toast.makeText(MainActivity.this, "[" + searchInput + "]을 검색합니다" , Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_SHORT).show();
+
+
+
+            }
+        });
+
+
     }
 }
